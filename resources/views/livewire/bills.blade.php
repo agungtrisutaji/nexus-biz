90aa32af0bd7301031affb0db31a314f4bff8c91
Agung Trisutaji <agung.trisutaji.aprian@gmail.com.com>
2024-05-12 00:22:14 +0700

    implement new layout

--- resources/views/livewire/bills.blade.php
+++ resources/views/livewire/bills.blade.php
@@ -1,5 +1,11 @@
+@extends('components.layouts.app')
+
+@section('content_body')
+@section('content_header_title', 'Operation')
+@section('content_header_subtitle', 'Bill')
 <div class="card">
     <div class="card-body">
         <h5 class="card-title">Bills</h5>
     </div>
 </div>
+@endsection
--- resources/views/livewire/forecast.blade.php
+++ resources/views/livewire/forecast.blade.php
@@ -1,5 +1,11 @@
+@extends('components.layouts.app')
+
+@section('content_body')
+@section('content_header_title', 'Operation')
+@section('content_header_subtitle', 'Forecast')
 <div class="card">
     <div class="card-body">
         <h5 class="card-title">Forecast</h5>
     </div>
 </div>
+@endsection
